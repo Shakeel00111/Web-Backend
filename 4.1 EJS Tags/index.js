8f7cbd002ce7ba63ed2dec063f1a03072b9e2fbc
[ms] <[namohammedshakeel21@gmail.com]>
2024-07-08 22:54:53 +0530

    Updating Web-Backend

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -15,3 +15,22 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// import express from "express";
+
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   const data = {
+//     title: "EJS Tags",
+//     seconds: new Date().getSeconds(),
+//     items: ["apple", "banana", "cherry"],
+//     htmlContent: "<em>This is some em text</em>",
+//   };
+//   res.render("solution.ejs", data);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
